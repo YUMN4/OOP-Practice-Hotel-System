2fdb681913a2dd72f89aa28448f63b6f0b65e296
YUMN4 <yumnahamadeh02@gmail.com>
2021-05-16 19:14:33 -0700

    Create and implement Room class.

---
+++ src/Room.java
@@ -0,0 +1,45 @@
+public class Room {
+
+    private int roomID;
+    private int numOfBeds;
+    private double costPerDay;
+
+    public Room(int roomID, int numOfBeds, double costPerDay){
+        this.roomID = roomID;
+        this.numOfBeds = numOfBeds;
+        this.costPerDay = costPerDay;
+    }
+
+
+    public void setRoomID(int newRoomID){
+        this.roomID = newRoomID;
+    }
+
+    public void setNumOfBeds(int newNumOfBeds){
+        this.numOfBeds = newNumOfBeds;
+    }
+
+    public void setCostPerDay(double newCostPerDay){
+        this.costPerDay = newCostPerDay;
+    }
+
+    public int getRoomID(){
+        return this.roomID;
+    }
+
+    public int getNumOfBeds(){
+        return this.numOfBeds;
+    }
+
+    public double getCostPerDay(){
+        return this.costPerDay;
+    }
+
+    @Override
+    public String toString(){
+        String roomInfo = "Room ID: " + this.roomID +
+                "\nNumber of beds: " + this.numOfBeds +
+                "\nCost per day: " + this.costPerDay;
+        return roomInfo;
+    }
+}
